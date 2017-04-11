7d5670ceaf76f43f10fb536c1ea90ca2c9415bc8
Zsolt Makrai <mecrai@gmail.com>
2017-04-11 15:06:19 +0200

    feat(game): draw one floor tile

--- the_wanderer.py
+++ the_wanderer.py
@@ -2,20 +2,22 @@ from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width='600', height='600')
+canvas = Canvas(root, width='720', height='720', background="black")
 canvas.pack()
 
 class FloorTile(object):
-    def __init__(self, x = 0, y = 0, size = 60):
+    def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
-        self.size = size
-        # self.background = PhotoImage(file = "/Users/MrFox/OneDrive/greenfox/the_wanderer_rpg/img/floor.png")
+        self.background = PhotoImage(file = "/Users/MrFox/OneDrive/greenfox/the_wanderer_rpg/img/floor.gif")
         self.walk_on = True
 
     def draw_floor_tile(self):
-        floor = canvas.create_rectangle(self.x,self.y, self.x+self.size,self.y+self.size)
+        canvas.create_image(0, 0, anchor="nw", image=self.background)
 
+
+# self.horizon = PhotoImage(file = './assets/floor.png')
+# 		self.canvas.create_image(0, self.height//4 * 3, anchor="nw", image=self.horizon)
 
 floormap = FloorTile()
 floormap.draw_floor_tile()
