e7b3053c07b0012c71b3a8f26c60d1c01919d137
Zsolt Makrai <mecrai@gmail.com>
2017-04-13 15:12:42 +0200

    refactor(game): hero jamps over the wall, again...

--- the_wanderer.py
+++ the_wanderer.py
@@ -19,50 +19,64 @@ class GameLogic(object):
         self.root.mainloop()
 
     def key_press(self, e):
-        self.e = e
+        draw_direction = None
         if e.keycode == 8320768: # up
-            if self.hero.y > 0:
-                self.hero.y -= 1
-                if self.floormap.get_tile_status(map_1, self.hero.x, self.hero.y) == True:
-                    self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_up)
-                else:
-                    self.hero.y += 1
-                    self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_up)
+            self.hero.y -= 1
+            draw_direction = self.hero.character_img_up
         elif e.keycode == 8255233: # down
-            if self.hero.y < 9:
-                self.hero.y += 1
-                if self.floormap.get_tile_status(map_1, self.hero.x, self.hero.y) == True:
-                    self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_down)
-                else:
-                    self.hero.y -= 1
-                    self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_down)
+            self.hero.y += 1
+            draw_direction = self.hero.character_img_down
         elif e.keycode == 8189699: # right
-            if self.hero.x < 9:
-                self.hero.x += 1
-                if self.floormap.get_tile_status(map_1, self.hero.x, self.hero.y) == True:
-                    self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_right)
-                else:
-                    self.hero.x -= 1
-                    self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_right)
+            self.hero.x += 1
+            draw_direction = self.hero.character_img_right
         elif e.keycode == 8124162: # left
-            if self.hero.x > 0:
-                self.hero.x -= 1
-                if self.floormap.get_tile_status(map_1, self.hero.x, self.hero.y) == True:
-                    self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_left)
-                else:
-                    self.hero.x += 1
-                    self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_left)
+            self.hero.x -= 1
+            draw_direction = self.hero.character_img_left
 
-    # def can_move(self):
-    #     if floormap.get_tile_status() == True:
-    #         hero.move_hero()
+        if self.floormap.get_tile_status(self.hero.x, self.hero.y) == True:
+            self.hero.draw_character(self.hero.x, self.hero.y, draw_direction)
+
+
+
+        #     if self.hero.y > 0:
+        #         self.hero.y -= 1
+        #         if self.floormap.get_tile_status(map_1, self.hero.x, self.hero.y) == True:
+        #             self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_up)
+        #         else:
+        #             self.hero.y += 1
+        #             self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_up)
+        # elif e.keycode == 8255233: # down
+        #     if self.hero.y < 9:
+        #         self.hero.y += 1
+        #         if self.floormap.get_tile_status(map_1, self.hero.x, self.hero.y) == True:
+        #             self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_down)
+        #         else:
+        #             self.hero.y -= 1
+        #             self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_down)
+        # elif e.keycode == 8189699: # right
+        #     if self.hero.x < 9:
+        #         self.hero.x += 1
+        #         if self.floormap.get_tile_status(map_1, self.hero.x, self.hero.y) == True:
+        #             self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_right)
+        #         else:
+        #             self.hero.x -= 1
+        #             self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_right)
+        # elif e.keycode == 8124162: # left
+        #     if self.hero.x > 0:
+        #         self.hero.x -= 1
+        #         if self.floormap.get_tile_status(map_1, self.hero.x, self.hero.y) == True:
+        #             self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_left)
+        #         else:
+        #             self.hero.x += 1
+        #             self.hero.draw_character(self.hero.x, self.hero.y, self.hero.character_img_left)
 
 class Map(object):
     def __init__(self, canvas):
         self.canvas = canvas
         self.floor = PhotoImage(file = "/Users/MrFox/OneDrive/greenfox/the_wanderer_rpg/img/floor.gif")
         self.wall = PhotoImage(file = "/Users/MrFox/OneDrive/greenfox/the_wanderer_rpg/img/wall.gif")
-        self.map_display(map_1)
+        self.level_map = map_1
+        self.map_display(self.level_map)
 
 
     def draw_floor_tile(self, x, y):
@@ -80,8 +94,8 @@ class Map(object):
                 else:
                     self.draw_wall_tile(row*tile, cell*tile)
 
-    def get_tile_status(self, board, x, y):
-        if board[y][x] == 0:
+    def get_tile_status(self, x, y):
+        if self.level_map[y][x] == 0:
             return True
         else:
             return False
@@ -107,8 +121,5 @@ class Hero(Character):
         self.character_img_left = PhotoImage(file = "/Users/MrFox/OneDrive/greenfox/the_wanderer_rpg/img/hero-left.gif")
 
         self.draw_character(self.x, self.y, self.character_img_down)
-
-    def move_hero(self):
-        pass
 
 game = GameLogic("720", "720")
